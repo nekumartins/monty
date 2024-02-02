7d0415c7833bcf70ad8aea714c6410cef1e9c59f
Chukwuneku <akpotohwoo@gmail.com>
2024-02-02 06:01:59 +0100

    quick fix

--- monty.h
+++ monty.h
@@ -35,7 +35,7 @@ typedef struct instruction_s
 } instruction_t;
 
 void push(stack_t **stack, char *arg, unsigned int line_number);
-void pall(stack_t *stack);
+void pall(stack_t *stack, unsigned int line_number);
 
 
 
--- pall.c
+++ pall.c
@@ -3,11 +3,13 @@
 /**
  * pall - Prints all the values on the stack
  * @stack: Pointer to the top of the stack.
+ * @line_number: Line number of the command in the Monty file.
  */
-void pall(stack_t *stack)
+void pall(stack_t *stack, unsigned int line_number)
 {
 	/*Traverse the stack and print all values*/
 	stack_t *current = stack;
+	(void)line_number;
 
 	while (current != NULL)
 	{
