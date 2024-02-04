ec1a25f292bbbdad27929d7456f95b9ff1590236
Favour <ofavourite001@gmail.com>
2024-02-04 16:05:22 +0100

    Fa

--- pall.c
+++ pall.c
@@ -1,19 +1,21 @@
 #include "monty.h"
-
 /**
- * pall - Prints all the values on the stack
- * @stack: Pointer to the top of the stack.
- * @line_number: Line number of the command in the Monty file.
- */
-void pall(stack_t *stack, unsigned int line_number)
+ * f_pall - prints the stack
+ * @head: stack head
+ * @counter: no used
+ * Return: no return
+*/
+void f_pall(stack_t **head, unsigned int counter)
 {
-	/*Traverse the stack and print all values*/
-	stack_t *current = stack;
-	(void)line_number;
+	stack_t *h;
+	(void)counter;
 
-	while (current != NULL)
+	h = *head;
+	if (h == NULL)
+		return;
+	while (h)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		printf("%d\n", h->n);
+		h = h->next;
 	}
 }
--- push.c
+++ push.c
@@ -1,50 +1,37 @@
 #include "monty.h"
-
 /**
- * push - Pushes an integer to the stack.
- * @stack: Double pointer to the stack.
- * @arg: The integer to be pushed.
- * @line_number: The line number of the push operation.
- */
-
-void push(stack_t **stack, char *arg, unsigned int line_number)
+ * f_push - add node to the stack
+ * @head: stack head
+ * @counter: line_number
+ * Return: no return
+*/
+void f_push(stack_t **head, unsigned int counter)
 {
-	int i;
-	stack_t *new_node;
+	int n, j = 0, flag = 0;
 
-	/*Check if arg is NULL*/
-	if (arg == NULL)
+	if (bus.arg)
 	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-
-	/*Check if arg is a valid integer*/
-	for (i = 0; arg[i] != '\0'; i++)
-	{
-		if (!isdigit(arg[i]))
+		if (bus.arg[0] == '-')
+			j++;
+		for (; bus.arg[j] != '\0'; j++)
 		{
-			fprintf(stderr, "L%u: usage: push integer\n", line_number);
-			exit(EXIT_FAILURE);
-		}
-	}
-
-	/*Create a new */
-	new_node = malloc(sizeof(stack_t));
-
-	if (new_node == NULL)
-	{
-		fprintf(stderr, "Error: malloc failed\n");
-		exit(EXIT_FAILURE);
-	}
-
-	/* Set the new node's value and next pointer */
-	new_node->n = atoi(arg);
-	new_node->next = *stack;
-	new_node->prev = NULL;
-
-	/* Update the head of the stack */
-	if (*stack != NULL)
-		(*stack)->prev = new_node;
-	*stack = new_node;
+			if (bus.arg[j] > 57 || bus.arg[j] < 48)
+				flag = 1; }
+		if (flag == 1)
+		{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+			fclose(bus.file);
+			free(bus.content);
+			free_stack(*head);
+			exit(EXIT_FAILURE); }}
+	else
+	{ fprintf(stderr, "L%d: usage: push integer\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE); }
+	n = atoi(bus.arg);
+	if (bus.lifi == 0)
+		addnode(head, n);
+	else
+		addqueue(head, n);
 }
